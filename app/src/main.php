d1c430aa2c455d4f5e992d63ae9da458796eb660
Aayush Rijal <arijal@aiims.com.au>
2022-10-05 17:00:30 +1100

    Banner done

---
+++ app/src/main.php
@@ -0,0 +1,92 @@
+<?php
+include __DIR__ . '/../functions.php';
+include __DIR__ . '/header.php';
+?>
+
+<header>
+    <div class="header-top-bar">
+        <div class="container">
+            <div class="row justify-content-center gx-xl-2">
+                <div class="col-auto">Rapid Response Time</div>
+                <div class="col-auto">
+                    <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11 14.8125C13.6579 14.8125 15.8125 12.6579 15.8125 10C15.8125 7.34213 13.6579 5.1875 11 5.1875C8.34213 5.1875 6.1875 7.34213 6.1875 10C6.1875 12.6579 8.34213 14.8125 11 14.8125Z" fill="white" />
+                        <path d="M6.50856 18.5154C4.95908 17.6974 3.66216 16.4721 2.75752 14.9716C1.85288 13.4711 1.37481 11.7521 1.37481 10C1.37481 8.24788 1.85288 6.52895 2.75752 5.02842C3.66216 3.52789 4.95908 2.30263 6.50856 1.48464C6.59177 1.44448 6.66604 1.38795 6.72692 1.31844C6.7878 1.24893 6.83404 1.16786 6.86288 1.08008C6.89173 0.992292 6.90258 0.899594 6.89479 0.807522C6.887 0.71545 6.86073 0.625892 6.81755 0.544201C6.77437 0.46251 6.71516 0.390361 6.64347 0.332066C6.57178 0.273771 6.48907 0.230526 6.40029 0.204915C6.31151 0.179304 6.21848 0.171852 6.12675 0.183005C6.03502 0.194158 5.94649 0.223687 5.86643 0.269828C4.0951 1.20383 2.61234 2.60365 1.57802 4.31834C0.543701 6.03302 -0.00292969 7.99752 -0.00292969 10C-0.00292969 12.0025 0.543701 13.967 1.57802 15.6817C2.61234 17.3964 4.0951 18.7962 5.86643 19.7302C5.94649 19.7763 6.03502 19.8059 6.12675 19.817C6.21848 19.8282 6.31151 19.8207 6.40029 19.7951C6.48907 19.7695 6.57178 19.7263 6.64347 19.668C6.71516 19.6097 6.77437 19.5375 6.81755 19.4558C6.86073 19.3741 6.887 19.2846 6.89479 19.1925C6.90258 19.1004 6.89173 19.0077 6.86288 18.92C6.83404 18.8322 6.7878 18.7511 6.72692 18.6816C6.66604 18.6121 6.59177 18.5556 6.50856 18.5154Z" fill="white" />
+                        <path d="M16.1336 0.269828C16.0535 0.223687 15.965 0.194158 15.8732 0.183005C15.7815 0.171852 15.6885 0.179304 15.5997 0.204915C15.5109 0.230526 15.4282 0.273771 15.3565 0.332066C15.2848 0.390361 15.2256 0.46251 15.1824 0.544201C15.1393 0.625892 15.113 0.71545 15.1052 0.807522C15.0974 0.899594 15.1083 0.992292 15.1371 1.08008C15.1659 1.16786 15.2122 1.24893 15.2731 1.31844C15.3339 1.38795 15.4082 1.44448 15.4914 1.48464C17.0409 2.30263 18.3378 3.52789 19.2425 5.02842C20.1471 6.52895 20.6252 8.24788 20.6252 10C20.6252 11.7521 20.1471 13.4711 19.2425 14.9716C18.3378 16.4721 17.0409 17.6974 15.4914 18.5154C15.4082 18.5556 15.3339 18.6121 15.2731 18.6816C15.2122 18.7511 15.1659 18.8322 15.1371 18.92C15.1083 19.0077 15.0974 19.1004 15.1052 19.1925C15.113 19.2846 15.1393 19.3741 15.1824 19.4558C15.2256 19.5375 15.2848 19.6097 15.3565 19.668C15.4282 19.7263 15.5109 19.7695 15.5997 19.7951C15.6885 19.8207 15.7815 19.8282 15.8732 19.817C15.965 19.8059 16.0535 19.7763 16.1336 19.7302C17.9049 18.7962 19.3876 17.3964 20.422 15.6817C21.4563 13.967 22.0029 12.0025 22.0029 10C22.0029 7.99752 21.4563 6.03302 20.422 4.31834C19.3876 2.60365 17.9049 1.20383 16.1336 0.269828Z" fill="white" />
+                    </svg>
+                </div>
+                <div class="col-auto">24/7 Emergency tree Removal available</div>
+            </div>
+        </div>
+    </div>
+    <div class="navigation-bar">
+        <div class="container">
+            <div class="row justify-content-center justify-content-lg-between align-items-center">
+                <div class="col-auto">
+                    <a href="./" class="d-flex align-items-center text-white text-decoration-none">
+                        <div class="fs-3">K&amp;A</div>
+                        <div class="ps-1 fs-11">Tree lopping Services</div>
+                    </a>
+                </div>
+                <div class="col-auto">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-auto ml-lg-3 mr-2 mr-lg-0">
+                            <a href="tel:<?= $phone_number ?>" class="btn fs-7 rounded-0 text-white">
+                                <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M20.7156 17.7579L16.4411 15.859C15.9969 15.6604 15.4995 15.6144 15.0265 15.7283C14.5535 15.8421 14.1315 16.1094 13.8263 16.4883L12.5945 18.0285C10.5749 16.7326 8.85572 15.0198 7.55221 13.0051L9.09242 11.7733C9.47103 11.4682 9.73806 11.0465 9.8519 10.5738C9.96573 10.1011 9.91996 9.60406 9.72172 9.16011L7.83382 4.87458C7.62203 4.39738 7.24761 4.01093 6.77735 3.78415C6.30708 3.55738 5.77155 3.50502 5.26627 3.63643L1.6478 4.58038C1.12342 4.71685 0.667753 5.04183 0.36793 5.49316C0.0681063 5.94449 -0.054831 6.4905 0.0226276 7.02678C0.696923 11.7068 2.86873 16.0425 6.21299 19.385C9.55725 22.7276 13.8941 24.8972 18.5744 25.5691C19.1108 25.6451 19.6564 25.5207 20.1068 25.2197C20.5573 24.9187 20.881 24.4623 21.0161 23.9377L21.96 20.3192C22.0913 19.814 22.0381 19.2786 21.81 18.8091C21.5819 18.3396 21.1939 17.9669 20.7156 17.7579Z" fill="white" />
+                                    <path d="M15.7331 13.0051C15.7331 13.4224 15.8989 13.8226 16.1939 14.1176C16.489 14.4126 16.8891 14.5784 17.3064 14.5784C17.7236 14.5784 18.1238 14.4126 18.4188 14.1176C18.7139 13.8226 18.8796 13.4224 18.8796 13.0051C18.8796 11.3361 18.2166 9.73548 17.0364 8.55531C15.8563 7.37514 14.2556 6.71213 12.5866 6.71213C12.1694 6.71213 11.7692 6.87789 11.4742 7.17293C11.1791 7.46797 11.0134 7.86813 11.0134 8.28538C11.0134 8.70264 11.1791 9.1028 11.4742 9.39784C11.7692 9.69288 12.1694 9.85863 12.5866 9.85863C13.4211 9.85863 14.2214 10.1901 14.8115 10.7802C15.4016 11.3703 15.7331 12.1706 15.7331 13.0051Z" fill="white" />
+                                    <path d="M12.5866 0.419128C12.1694 0.419128 11.7692 0.584881 11.4742 0.879923C11.1791 1.17496 11.0134 1.57513 11.0134 1.99238C11.0134 2.40963 11.1791 2.80979 11.4742 3.10484C11.7692 3.39988 12.1694 3.56563 12.5866 3.56563C15.0894 3.56813 17.4889 4.56345 19.2586 6.33316C21.0283 8.10287 22.0236 10.5024 22.0261 13.0051C22.0261 13.4224 22.1919 13.8226 22.4869 14.1176C22.782 14.4126 23.1821 14.5784 23.5994 14.5784C24.0166 14.5784 24.4168 14.4126 24.7118 14.1176C25.0069 13.8226 25.1726 13.4224 25.1726 13.0051C25.1689 9.66827 23.8417 6.46915 21.4821 4.10962C19.1226 1.7501 15.9235 0.422876 12.5866 0.419128Z" fill="white" />
+                                </svg>
+                                <span class="ps-3">call us today <?= $phone_number ?></span>
+                            </a>
+                        </div>
+                        <div class="col-12 col-md-auto ml-lg-2">
+                            <a href="#form" class="btn btn-white rounded-3 text-primary fs-14 px-md-5">
+                                Book Online
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="fs-1 bg-primary d-inline-flex line-height-1 pe-3">The problem solver</div>
+                <div class="fs-1 text-white line-height-1 pe-3">Tree lopper</div>
+                <div class="text-white letter-spacing-12 fs-10">We offer a wide selection of expert tree work and arborist services</div>
+                <div class="font-articulat fs-14 text-white pe-xxl-5 pt-2 pb-5">Welcome to K&A Tree Lopping Services. Are you looking for a reliable and high-quality tree surgeon in the Sydney area? Then look no further. We offer a wide selection of expert tree work and arborist services to private residents, local authorities and larger companies. We have been exceeding our customer’s expectations since 2015.</div>
+                <div class="row text-white gx-2">
+                    <div class="col h-inherit">
+                        <div class="bg-primary text-end line-height-1 p-3 h-100 d-flex flex-column justify-content-center position-relative">
+                            <?= renderImg('tree-lopper-guy.png','lib', 'tree-lopper-guy') ?>
+                            <div class="fs-5">Refer a Neighbour</div>
+                            <div class="fs-7 letter-spacing-34">Receive 10% off</div>
+                        </div>
+                    </div>
+                    <div class="col-auto h-inherit">
+                        <div class="bg-primary text-center line-height-1 p-3 h-100">
+                            <div class="fs-8">Refer</div>
+                            <div class="fs-4">2</div>
+                            <div class="fs-8">15% off</div>
+                        </div>
+                    </div>
+                    <div class="col-auto h-inherit">
+                        <div class="bg-primary text-center line-height-1 p-3 h-100">
+                            <div class="fs-8">Refer</div>
+                            <div class="fs-4">3</div>
+                            <div class="fs-8">20% off</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?= renderImg('white-trees.png', 'background', 'white-trees') ?>
+</section>
+
+<?php include __DIR__ . '/footer.php'; ?>
